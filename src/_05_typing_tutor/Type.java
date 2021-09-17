20da26bacb3c671a21422dd4a08d0c411cd92994
league <league@imac-16.attlocal.net>
2021-09-17 16:50:15 -0700

    book

--- src/_05_typing_tutor/Type.java
+++ src/_05_typing_tutor/Type.java
@@ -1,18 +1,67 @@
 package _05_typing_tutor;
 
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Random;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Type {
+public class Type implements KeyListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
 	char currentLetter;
+	char generateRandomLetter() {
+	    Random r = new Random();
+	    return (char) (r.nextInt(26) + 'a');
+	}
+
 void setup() {
 	frame.setVisible(true);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	currentLetter=generateRandomLetter;
-	JLabel label = new JLabel();
-	label.setText("");
+	currentLetter=generateRandomLetter();
+
+	label.setText(String.valueOf(currentLetter));
+	label.setFont(label.getFont().deriveFont(28.0f));
+	label.setHorizontalAlignment(JLabel.CENTER);
 	//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
+	frame.addKeyListener(this);
+	panel.add(label);
+	frame.add(panel);
+	
+	frame.setSize(100, 100);
+}
+public static void main(String[] args){
+	Type tutor = new Type();
+	tutor.setup(); 
+}
+
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void keyPressed(KeyEvent e) {
+	// TODO Auto-generated method stub
+	if(e.getKeyChar()==currentLetter) {
+		System.out.println("correct");
+		panel.setBackground(Color.GREEN);
+	}
+	else {
+		panel.setBackground(Color.RED);
+	}
+	currentLetter=generateRandomLetter();
+	label.setText(String.valueOf(currentLetter));
+	System.out.println(currentLetter);
+	
+}
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
 }
--- src/_05_typing_tutor/Type_goes_on_a_run.java
+++ src/_05_typing_tutor/Type_goes_on_a_run.java
@@ -4,10 +4,6 @@ import java.util.Random;
 
 public class Type_goes_on_a_run {
 public static void main(String[] args) {
-	char generateRandomLetter() {
-	    Random r = new Random();
-	    return (char) (r.nextInt(26) + 'a');
+	new Type.setup();
 	}
-
-}
 }
